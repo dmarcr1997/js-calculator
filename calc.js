e03b51b48e7e15b9569a0620fc976ae3a07be2b7
dmarc <dmarcr1997@gmail.com>
2020-06-30 22:20:02 -0500

    created Calculator class

---
+++ calc.js
@@ -0,0 +1,35 @@
+class Calc {
+    static add(nums){
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+        return nums.reduce(reducer, 0)
+    }
+    static subtract(nums){
+        let difference = nums[0]
+        for(let i = 1; i < nums.length; i++){
+            difference -= nums[i]
+        }
+        return difference
+    }
+    static multiply(nums){
+        let product = nums[0]
+        for(let i = 1; i < nums.length; i++){
+            product *= nums[i]
+        }
+        return product
+    }
+    static divide(num1, num2){
+        if (num2 === 0)
+            alert("You cannot divide by Zero!")
+        else
+            return num1 / num2
+    }
+}
+
+let renderCalc = () => {
+    
+}
+
+
+document.addEventListener('DOMContentLoaded', (event) = > {
+    renderCalc()
+})
